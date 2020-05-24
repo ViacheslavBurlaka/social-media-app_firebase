e215e1a8f950c40969110be22cdff538873a0b41
Viacheslav Burlaka <viacheslav.burlaka@uvoteam.com>
2020-05-24 20:09:54 +0300

    05 added login route

--- functions/index.js
+++ functions/index.js
@@ -150,4 +150,45 @@ app.post('/signup', (req, res) => {
     })
 })
 
+// Login route
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let errors = {};
+
+  if (isEmpty(user.email)) {
+    errors.email = 'Must not be empty'
+  }
+
+  if (isEmpty(user.password)) {
+    errors.password = 'Must not be empty'
+  }
+
+  if (Object.keys(errors).length) {
+    return res.status(400).json(errors)
+  }
+
+  // if no errors
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken()
+    })
+    .then(token => {
+      return res.json({token})
+    })
+    .catch(err => {
+      console.error(err)
+      if (err.code === 'auth/wrong-password') {
+        return res.status(403).json({general: 'Wrong credentials, please try again'})
+      } else {
+        return res.status(500).json({error: err.code})
+      }
+    });
+})
+
 exports.api = functions.https.onRequest(app);
