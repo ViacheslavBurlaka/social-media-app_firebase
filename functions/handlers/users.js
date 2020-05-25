cba8d5d5c8f92252328a4e73ba424496e7d4ba1d
Viacheslav Burlaka <burlakaviacheslav@gmail.com>
2020-05-26 02:03:52 +0300

    update handling errors at users routes

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -72,7 +72,7 @@ exports.signup = (req, res) => {
       } else {
         return res
           .status(500)
-          .json({error: err.code})
+          .json({general: "Something went wrong, please try again"});
       }
     })
 }
@@ -104,10 +104,10 @@ exports.login = (req, res) => {
     })
     .catch(err => {
       console.error(err)
-      if (err.code === 'auth/wrong-password') {
-        return res.status(403).json({general: 'Wrong credentials, please try again'})
-      } else {
-        return res.status(500).json({error: err.code})
-      }
+      // auth/wrong-password
+      // auth/user-not-user
+      return res
+        .status(403)
+        .json({general: "Wrong credentials, please try again"});
     });
 }
