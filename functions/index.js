94f52007e8c231c545edbe314a626939a6ff783a
Viacheslav Burlaka <burlakaviacheslav@gmail.com>
2020-06-02 15:27:15 +0300

    added functions-triggers on change image and on delete post

--- functions/index.js
+++ functions/index.js
@@ -69,6 +69,7 @@ exports.createNotificationOnLike = functions
       })
       .catch((err) => console.error(err));
   });
+
 exports.deleteNotificationOnUnLike = functions
   .region('europe-west1')
   .firestore.document('likes/{id}')
@@ -81,6 +82,7 @@ exports.deleteNotificationOnUnLike = functions
         return;
       });
   });
+
 exports.createNotificationOnComment = functions
   .region('europe-west1')
   .firestore.document('comments/{id}')
@@ -107,4 +109,66 @@ exports.createNotificationOnComment = functions
         console.error(err);
         return;
       });
+  });
+
+// if user changes his own profile image it will trigger to change this photo at all places
+exports.onUserImageChange = functions
+  .region('europe-west1')
+  .firestore.document('/users/{userId}')
+  .onUpdate((change) => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log('image has changed');
+      const batch = db.batch();
+      return db
+        .collection('screams')
+        .where('userHandle', '==', change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const scream = db.doc(`/screams/${doc.id}`);
+            batch.update(scream, {userImage: change.after.data().imageUrl});
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
+
+// if user deletes his post, it will trigger to delete all information about this post (comments, likes, notifications)
+exports.onScreamDelete = functions
+  .region('europe-west1')
+  .firestore.document('/screams/{screamId}')
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = db.batch();
+    return db
+      .collection('comments')
+      .where('screamId', '==', screamId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db
+          .collection('likes')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db
+          .collection('notifications')
+          .where('screamId', '==', screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
   });
