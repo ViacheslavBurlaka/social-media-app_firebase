77ae66555dfafc3f9025c1b93e8d9d7d3f1be2f8
Viacheslav Burlaka <burlakaviacheslav@gmail.com>
2020-06-05 23:45:45 +0300

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+const cors = require('cors');
+app.use(cors());
+
 const {db} = require('./utils/admin');
 
 const {
